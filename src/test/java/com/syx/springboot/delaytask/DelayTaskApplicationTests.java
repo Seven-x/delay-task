eb2d44b70af6c4b57590733ec2a23e672fb98481
Seven-X <1098899894@qq.com>
2020-04-18 09:44:29 +0800

    feat: 初始项目

---
+++ src/test/java/com/syx/springboot/delaytask/DelayTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.springboot.delaytask;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
